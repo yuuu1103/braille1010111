458bbe05066ec1ee7a9882d777e77d1f662bf41e
Denny Huang <denny0223@gmail.com>
2025-07-03 22:03:49 +0800

    feat: configure basePath and assetPrefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/braille1010111',
+  assetPrefix: '/braille1010111/',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
