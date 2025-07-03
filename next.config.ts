43d0a95322277d32cced5b17280e52d8b0bfb853
Denny Huang <denny0223@gmail.com>
2025-07-03 21:43:59 +0800

    feat: Add GitHub Actions workflow for deploying to GitHub Pages
    This commit introduces a GitHub Actions workflow to automatically build and deploy the Next.js application to GitHub Pages.
    
    It also updates `next.config.ts` to support static exports for the deployment.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -9,6 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
